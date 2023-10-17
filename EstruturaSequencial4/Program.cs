eba0caa063bfe44cd4b70dd8064f1bf47ad154f1
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:47:30 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaSequencial4/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+int numFunc = int.Parse(Console.ReadLine());
+int horaTrab = int.Parse(Console.ReadLine());
+double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double salario = horaTrab * valorPorHora;
+
+Console.WriteLine("NUMBER = " + numFunc);
+Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
